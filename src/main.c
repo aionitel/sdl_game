716bea78ddd31a612cc5934a584ab27684268988
Alex Ionitel <oranggsoda@gmail.com>
2024-08-28 16:19:10 -0400

    rainbow triangle

--- src/main.c
+++ src/main.c
@@ -2,27 +2,34 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h> #include <string.h>
+#include <assert.h> 
+#include <string.h>
+
 static const int WIDTH = 1080;
 static const int HEIGHT = 720;
 
 static float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f,  0.5f, 0.0f
+	// position        // colour
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom-right corner
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom-left corner
+    0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f // Top corner
 };
 
 static const char *vertex_shader_source =
 	"#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
 	"void main() {\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"	ourColor = aColor;\n"
 	"}\0";
 static const char *fragment_shader_source =
 	"#version 330 core\n"
+	"in vec3 ourColor;\n"
 	"out vec4 FragColor;\n"
 	"void main() {\n"
-	"	FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"FragColor = vec4(ourColor, 1.0);\n"
 	"}\0";
 
 unsigned int get_shader_program() {
@@ -51,12 +58,12 @@ unsigned int get_shader_program() {
 		printf("Fragment shader could not be compiled!\n");
 		printf("Fragment shader ERROR: %s\n", log);
 	}
-
 	// Link shaders.
 	unsigned int shader_program = glCreateProgram();
 	glAttachShader(shader_program, vertex_shader);
 	glAttachShader(shader_program, fragment_shader); glLinkProgram(shader_program);
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &success); glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length); log = realloc(log, log_length);
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success); glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length);
+	log = realloc(log, log_length);
 	if (!success) {
 		glGetShaderInfoLog(shader_program, log_length, NULL, log); printf("Error linking shaders with shader_program\n");
 		printf("LINKER ERROR: %s\n", log);
@@ -161,7 +168,12 @@ int main() {
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// Global draw state.
@@ -173,9 +185,7 @@ int main() {
 	SDL_Event event;
 	while (running) {
 		glClear(GL_COLOR_BUFFER_BIT);
-		glEnableVertexAttribArray(0);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDisableVertexAttribArray(0);
 		SDL_GL_SwapWindow(window); // Swap the buffers to display the current frame
 
 		if (SDL_PollEvent(&event)) {
