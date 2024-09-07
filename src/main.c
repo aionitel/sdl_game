bee85415017c569b2544bd1b5e04c12a1d2a3a81
Alex Ionitel <oranggsoda@gmail.com>
2024-09-07 13:20:22 -0400

    correct rgba values for rainbow square

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@ static const char *fragment_shader_source =
 static float vertices[] = {
 	// Top right.
     0.5f,  0.5f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
 	// Top left.
 	-0.5f, 0.5f, 0.0, 1.0f,
 	1.0f, 0.0f, 0.0f, 1.0f,
